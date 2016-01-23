387f9935c5e9447cbf46d12a8a83a6a2382e5c8f
ColinWhitmarsh <Colin.Whitmarsh@gmail.com>
2016-01-22 16:19:36 -0800

    enable correct number of enemies to randomly move every two seconds

--- client/watchout.js
+++ client/watchout.js
@@ -6,14 +6,33 @@ var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height)
     .append("g")
-    .attr("transform", "translate(0," + height / 2 + ")");
+    .attr("transform", "translate(0,0)");
 
 var enemyMaker = function() {
     d3.select("g").append('circle')
+    .attr('class', 'enemy')
     .attr('cx', Math.random() * width)
     .attr('cy', Math.random() * height)
-    .attr('r', 20)
+    .attr('r', 10)
     .attr('fill','green');
   };
 
-var intervalID = setInterval(function(){enemyMaker();}, 100);
+for ( var i = 0; i < numberOfEnemies; i++){
+  enemyMaker(i);
+}
+
+
+var enemyMover = function(enemyClass) {
+  var enemies = d3.selectAll('.enemy');
+  console.log(enemies[0].length);
+
+  for ( var i = 0; i < enemies[0].length; i++){
+    d3.select(enemies[0][i])
+    .transition()
+    .duration(2000)
+    .attr('cx', Math.random() * width)
+    .attr('cy', Math.random() * height);
+  }
+};
+
+setInterval(function(){enemyMover();}, 2000);
