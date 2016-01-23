8ccba34673837c2ee21a7a9ecaa4d7451e8ceca2
ColinWhitmarsh <Colin.Whitmarsh@gmail.com>
2016-01-22 19:25:52 -0800

    Make a differently-colored draggable dot to represent the player

--- client/watchout.js
+++ client/watchout.js
@@ -11,21 +11,39 @@ var svg = d3.select("body").append("svg")
 var enemyMaker = function() {
     d3.select("g").append('circle')
     .attr('class', 'enemy')
+    .attr('fill','red')
     .attr('cx', Math.random() * width)
     .attr('cy', Math.random() * height)
-    .attr('r', 10)
-    .attr('fill','green');
+    .attr('r', 10);
   };
 
-for ( var i = 0; i < numberOfEnemies; i++){
-  enemyMaker(i);
+var dragMove = function() {
+  d3.select(this)
+  .attr('cx', d3.event.x)
+  .attr('cy', d3.event.y);
+};
+
+var drag = d3.behavior.drag()
+  .on("drag", dragMove);
+
+var heroMaker = function() {
+    d3.select("g").append('circle')
+    .attr('class', 'hero')
+    .attr('fill','green')
+    .attr('cx', width / 2)
+    .attr('cy', height / 2)
+    .attr('r', 10)
+    .call(drag);
+  };
+
+for (var i = 0; i < numberOfEnemies; i++) {
+  enemyMaker();
 }
 
+heroMaker();
 
 var enemyMover = function(enemyClass) {
   var enemies = d3.selectAll('.enemy');
-  console.log(enemies[0].length);
-
   for ( var i = 0; i < enemies[0].length; i++){
     d3.select(enemies[0][i])
     .transition()
